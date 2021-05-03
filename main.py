20465fcf40e34a1b491131be316db84364286eb3
marcel1842 <64965375+marcel1842@users.noreply.github.com>
2021-05-03 16:44:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from discord.ext.commands import has_permissions
+import time
 
 client = commands.Bot(command_prefix = ".")
 client.remove_command("help")
@@ -11,8 +12,17 @@ async def on_ready():
 
 
 @client.command()
-async def help(ctx):
-    await ctx.send("Na razie jest tylko komenda help")
+async def help(ctx, value=None):
+    
+    if(value == "test"):
+        await ctx.send("test")
+    elif(value == "test2"):
+        await ctx.send("test2")
+    elif(value == None):
+        embed=discord.Embed(title="Help Commands", description="Categories", color=0x52fa0f)
+        embed.add_field(name="help test", value="pokazuje test", inline=False)
+        embed.add_field(name="help test2", value="pokazuje test2", inline=False)
+        await ctx.send(embed=embed)
 
 
 
@@ -20,7 +30,9 @@ async def help(ctx):
 @has_permissions(manage_messages = True)
 async def clear(ctx, value=5):
     await ctx.channel.purge(limit=value+1)
-
+    time.sleep(3)
+    embed=discord.Embed(title=f"usuniento {value}", color=0x52fa0f)
+    await ctx.send(embed=embed)
 
 @client.command()
 @has_permissions(ban_members=True)
@@ -32,6 +44,4 @@ async def kick(ctx, member : discord.Member, *, reason="No Reason"):
 async def ban(ctx, member : discord.Member, *, reason="No Reason"):
     await member.ban(reason=reason)
 
-
-
-client.run("token")
+client.run("twój token")
