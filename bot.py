cbf3ed43b70222e8106934766e4130d1109f6cac
marcel1842 <64965375+marcel1842@users.noreply.github.com>
2021-05-12 13:20:30 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,12 @@ async def on_ready():
 
 @client.command()
 async def help(ctx):
-    await ctx.channel.send("Pomoc")
+    embed=discord.Embed(title="Help ", url="https://www.youtube.com/channel/UC4vtx0j0wcP6s4n7hCTUs7A", description="Commands", color=0xeb1e1e)
+    embed.add_field(name="help", value="pokazuje okienko", inline=False)
+    embed.add_field(name="test", value="test", inline=False)
+    embed.add_field(name="test1", value="test1", inline=False)
+    await ctx.send(embed=embed, delete_after = 10)
+
 
 
 client.run("token")
